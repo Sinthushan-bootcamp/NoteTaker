0e4e93dbf0427e05a7b5e13d99bbfc09d5b55727
sinthushan <sinthushan@gmail.com>
2023-03-05 12:01:07 -0500

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # NoteTaker
+
+![MIT license](https://img.shields.io/badge/license-MIT-blue)
+
+## Description 
+
+Create and store notes. This app allows the user to create simple notes and store them for latter reading. THe app also allows the user to delete any notes that are no longer needed.
+
+![notes page hidden save button](./screenshots/notes_screenshot_one.PNG)
+![notes page with save after typing a note](./screenshots/notes_screenshot_two.PNG)
+
+## Table of Contents
+* [installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+
+
+## Installation
+To install necessary dependencies, run the following command:
+```
+npm i
+```
+## Usage
+First clone the repo and from the command line CD into the directory. Once in the directory follow the [install instructions](#installation) and then run either one of the following commands
+
+Use your own Port:
+```
+PORT=<your port number> node server.js
+``` 
+Use test port of 3001
+```
+node server.js
+``` 
+
+## License
+This project is licensed under the MIT.
--- server.js
+++ server.js
@@ -1,69 +1,74 @@
+// Requirements
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+//use unique port if provided otherwise use test port of 3001
 const PORT = process.env.PORT || 3001;
-
+// initiate express app
 const app = express();
+// middleware
+app.use(express.json()); //put parsed json request in body
+app.use(express.static('public')); // make assets available when hosting html files
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-
-// GET Route for homepage
+// GET Route for landing page
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html')) // return index.html
 );
-
+// GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html')) // return notes.html
 );
-
+// API GET Request to pull all notes
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', function read(err, data) {
         if (err) {
             res.err('file missing');
         }
-        res.json(JSON.parse(data))
+        res.json(JSON.parse(data)) //send json response with json data from db.json
     });
 });
-
+// API POST Request to add a new note
 app.post('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', function read(err, data) {
+    fs.readFile('./db/db.json', function read(err, data) { // get json data from db.json file
         if (err) {
-            res.err('file missing');
+            res.err('file missing'); // throw error if file is missing or corrupted 
         } else {
             console.log(data);
-            const parsedData = JSON.parse(data);
+            const parsedData = JSON.parse(data); // parse existing notes
+            // conditional to check if there is existing note data
             if (parsedData.length){
+                // if note data exist, get the last note made and increment it's id by one
+                // this incremented id will be used for the new note
                 const lastID = parsedData[parsedData.length-1].id
                 note_id = lastID + 1
             }else{
+                // if no notes exist assign the new note ID as 1
                 note_id = 1
             }
+            // deconstruct the request body object and then construct a content object with the req data and add ID
             const { title, text }  = req.body
             const content = {
                 id: note_id,
                 title,
                 text
             };
-            parsedData.push(content);
-            fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err) =>
-                err ? res.error('Error in adding note') : res.json('Added note')
+            parsedData.push(content);// push new note to existing notes parsed from file
+            fs.writeFile('./db/db.json', JSON.stringify(parsedData), (err) => // write the json file with the updated notes array
+                err ? res.error('Error in adding note') : res.json('Added note') // send response to complete promise and trigger callback functions
             );
         }
     });
 });
-
+// API DELETE Request to remove a note
 app.delete('/api/notes/:id', (req, res) => {
-    fs.readFile('./db/db.json', function read(err, data) {
+    fs.readFile('./db/db.json', function read(err, data) { // get json data from db.json file
         if (err) {
-            res.err('file missing');
+            res.err('file missing'); // throw error if file is missing or corrupted 
         }
-        const notes = JSON.parse(data);
-        newNotes = notes.filter(note => note.id != req.params.id)
-        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) =>
-            err ? res.error('Error in removing note') : res.json('Note Removed')
+        const notes = JSON.parse(data); // parse json data
+        newNotes = notes.filter(note => note.id != req.params.id) // use array filter method to remove the ID of the note to be removed
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err) => // write the json file with filtered notes array
+            err ? res.error('Error in removing note') : res.json('Note Removed') // send response to complete promise and trigger callback functions
         );
     });
 })
